67c1e808b5a80bd2213fd63a156694c3da380c10
zelihaarduclu <61326828+zelihaarduclu@users.noreply.github.com>
2021-02-26 14:27:36 +0200

    bilgiyarismasi

---
+++ Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bilgi_yarişması
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int dogrusayısı=0,yanlişsayisi=0;
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (radioButton5.Checked == true)
+            {
+                dogrusayısı = dogrusayısı + 1;
+                label2.Text = dogrusayısı.ToString();
+
+            }
+            else
+            {
+                yanlişsayisi = yanlişsayisi + 1;
+                label3.Text = yanlişsayisi.ToString();
+            }
+            button2.Enabled = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           if(radioButton2.Checked==true)
+            {
+                dogrusayısı = dogrusayısı + 1;
+                label2.Text = dogrusayısı.ToString();
+            }
+           else
+            {
+                yanlişsayisi = yanlişsayisi + 1;
+                label3.Text = yanlişsayisi.ToString();
+                
+            }
+          button1.Enabled=false;
+        }
+    }
+}
